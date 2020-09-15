9fc1cb0d6832f8db7db8614a51c4f7c25ce83a2a
kz131746 <kendra.zuleger@umconnect.umt.edu>
2020-09-15 12:04:13 -0600

    Now the link redirects as it should.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -17,3 +17,12 @@ class BlogDetailView(DetailView):
     model = Post
     
     template_name = 'post_detail.html'
+    
+
+class BlogCreateView(CreateView):
+
+    model = Post
+    
+    template_name = 'post_new.html'
+    
+    fields = ['title', 'author', 'body']
